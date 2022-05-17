19a8d4fa3b88aa0226f76f2e4edd21082542d8d2
letranthuan270797 <letranthuan.iuh@gmail.com>
2022-05-17 14:14:14 +0700

    aaa

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DatabasaProvider;
+using DatabaseIO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,17 +14,166 @@ namespace WebApplication1.Controllers
         public ActionResult Index()
         {
 
-            if (Session["user"]==null)
+            //if (Session["user"]==null)
+            //{
+            //    Response.Redirect("https://localhost:44328/login/index");
+            //}
+            //else
+            //{
+            //    //TVLT_Users user = (TVLT_User)Session["user"];
+            //    //ViewBag.Name = user.FullName;
+            //}
+
+
+            //DBIO db = new DBIO();
+            //kymdan_Users u = db.GetObject_User("admin", "admin");            
+            //return View(u);
+
+            DBIO db = new DBIO();
+            List<kymdan_Users> list = db.GetList_User();
+            return View(list);
+
+        }
+        /// <summary>
+        /// Them User moi
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult AddUser(FormCollection data)
+        {
+            string uid = data["uid"];
+            string pwd = data["pwd"];
+            string name = data["name"];
+            JsonResult js = new JsonResult();
+            if (string.IsNullOrEmpty(uid) ||
+                string.IsNullOrEmpty(pwd) ||
+                string.IsNullOrEmpty(name))
             {
-                Response.Redirect("https://localhost:44328/login/index");
+                js.Data = new
+                {
+                    status = "ERROR",
+                    message = "Khong the bo trong du lieu"
+                };
             }
             else
             {
-                //TVLT_Users user = (TVLT_User)Session["user"];
-                //ViewBag.Name = user.FullName;
+                DBIO db = new DBIO();
+                kymdan_Users u = new kymdan_Users();
+                u.ID = Guid.NewGuid().ToString();               
+                u.Uid = uid;
+                u.Pwd = pwd;
+                u.Fullname = name;
+
+                db.AddObject(u);
+                db.Save();
+                js.Data = new
+                {
+                    NewID = u.ID,
+                    status = "OK"
+
+                };               
             }
-            return View();
+            return Json(js, JsonRequestBehavior.AllowGet);            
+        }
+        /// <summary>
+        /// Xoa 1 ROW
+        /// </summary>      
+        [HttpPost]
+        public JsonResult DeleteUser(FormCollection data)
+        {
+            string id = data["id"];
+            
+            JsonResult js = new JsonResult();
+            if (string.IsNullOrEmpty(id) )
+            {
+                js.Data = new
+                {
+                    status = "ERROR",
+                    message = "Khong the bo trong du lieu"
+                };
+            }
+            else
+            {
+                DBIO db = new DBIO();
+
+                kymdan_Users u = db.GetObject_User(id);
+                if (u== null)
+                {
+                    js.Data = new
+                    {
+                        status = "ERROR",
+                        message = "Du lieu khong ton tai"
+                    };
+                }
+                else
+                {
+                    db.DeleteObject(u);
+                    db.Save();
+                    js.Data = new
+                    {
+                        status = "OK"
+                    };
+                }
+               
+
+               
+                
+            }
+            return Json(js, JsonRequestBehavior.AllowGet);
         }
 
+        /// <summary>
+        /// Cap nhat 1 ROW - EDIT ROW
+        /// </summary>        
+        [HttpPost]
+        public JsonResult EditUser(FormCollection data)
+        {
+            string id = data["id"];
+            string uid = data["uid"];
+            string pwd = data["pwd"];
+            string name = data["name"];
+            JsonResult js = new JsonResult();
+
+            if (string.IsNullOrEmpty(id)||
+                string.IsNullOrEmpty(uid)||
+                string.IsNullOrEmpty(name)
+                )
+            {
+                js.Data = new
+                {
+                    status = "ERROR",
+                    message = "Khong the bo trong du lieu(tru password)"
+                };
+            }
+            else
+            {
+                DBIO db = new DBIO();
+
+                kymdan_Users u = db.GetObject_User(id);
+                if (u == null)
+                {
+                    js.Data = new
+                    {
+                        status = "ERROR",
+                        message = "Du lieu khong ton tai"
+                    };
+                }
+                else
+                {
+                    u.Uid = uid;
+                    u.Fullname = name;
+                    if (!string.IsNullOrEmpty(pwd))
+                    {
+                        u.Pwd = pwd;
+                    }
+                    db.Save();
+                    js.Data = new
+                    {
+                        status = "OK"
+                    };
+                }
+            }
+            return Json(js, JsonRequestBehavior.AllowGet);
+        }
     }
 }
--- WebApplication1/Controllers/LoginController.cs
+++ WebApplication1/Controllers/LoginController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -56,13 +61,32 @@ namespace WebApplication1.Controllers
 
             //    }
             //}
-            if (uid == "admin"  && pwd == "123456")
+            if (uid == "admin" && pwd == "123456")
             {
                 Session["user"] = "admin";
                 Session.Timeout = 5;
+                
+                var acountid = ConfigurationManager.AppSettings["SMSAccountIdentification"];
+                var authToken = ConfigurationManager.AppSettings["SMSAccountPassword"];
+                TwilioClient.Init(acountid, authToken);
+                var to = new PhoneNumber("+84384443542");
+                var from = new PhoneNumber("+14094985275");
+                string otp =string.Empty;
+                Random rd = new Random();
+                for(int i = 0;i<5; i++)
+                {
+                    int temp = rd.Next(0, 10);
+                    otp += temp;
+                }
+                var mess = MessageResource.Create(
+                        to: to,
+                        from: from,
+                        body: otp
+                    );
                 jr.Data = new
                 {
-                    status = "OK"
+                    status = "OK",
+                    res = otp
                 };
             }
             else
@@ -71,9 +95,32 @@ namespace WebApplication1.Controllers
                 {
                     status = "F"
                 };
-                
+
             }
             return Json(jr, JsonRequestBehavior.AllowGet);
         }
+        [HttpGet]
+        public ActionResult GetOtp(string resOtp)
+        {
+            OtpModel model = new OtpModel
+            {
+                otpkey = resOtp
+            };
+            return PartialView("~/views/Login/OtpInput.cshtml", model);
+        }
+        [HttpPost]
+        public ActionResult GetOtp(OtpModel model)
+        {
+            if (model.otpkey.Equals(model.otpText))
+            {
+                Response.Redirect("https://localhost:44328/home/index");
+
+            }
+
+            ViewBag.mess = "Mã xác thực không hợp lệ";
+            return PartialView("~/views/Login/OtpInput.cshtml", model);
+        }
+
+
     }
 }
