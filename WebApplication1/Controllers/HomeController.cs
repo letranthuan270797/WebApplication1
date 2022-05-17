7e16978d15bf2fc542ece46902b04ea48d0512d1
Conan KLOP <36772030+ConanKlop@users.noreply.github.com>
2022-05-17 16:56:17 +0700

    a

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -13,8 +13,9 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
+            //3123123sdadsadasd
             //3123123
-            //
+            //asdasdasdasd
             //if (Session["user"]==null)
             //{
             //    Response.Redirect("https://localhost:44328/login/index");
